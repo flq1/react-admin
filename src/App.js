c9c3095c813cfb568eca81f1c4f7da64aa5e2af6
24kcs <2940266566@qq.com>
2020-01-12 17:41:13 +0800

    创建路由

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React ,{Component} from 'react'
+import {BrowserRouter as Router , Route } from 'react-router-dom'
+import Login from './components/login'
+import Home from './components/home'
+export default class App extends Component{
+    render(){
+        return (
+          <div>
+            <Router>
+              
+              <Route path="/" exact component={Login}></Route>
+              <Route path="/home" exact component={Home}></Route>
+            </Router>
+          </div>
+        );
+    }
+}
